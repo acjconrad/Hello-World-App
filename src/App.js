3ee14358ad01e3321478fa80642f533de3ae9844
acjconrad <104440864+acjconrad@users.noreply.github.com>
2022-04-28 09:13:15 -0400

    custom message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alachua County loves React
         </a>
       </header>
     </div>
